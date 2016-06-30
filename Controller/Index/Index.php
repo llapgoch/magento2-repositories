6f9d97770fad3dfa595ff5a5a29dc160fc23cb33
Andy Smart <asmart@iweb.co.uk>
2016-06-30 10:57:01 +0100

    Initial commit

---
+++ Controller/Index/Index.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Training\Repository\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+
+class Index extends Action
+{
+    public function execute()
+    {
+        // TODO: Implement execute() method.
+        die('Put your lovely stuff here');
+    }
+}
